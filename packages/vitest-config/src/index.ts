e467dd952120f781899072c2c279c1730c926012
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2024-12-20 18:44:24 +0100

    feat(vitest-config)!: use v8 for coverage + add coverage path patterns

--- packages/vitest-config/src/index.ts
+++ packages/vitest-config/src/index.ts
@@ -5,7 +5,19 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul',
+      include: [
+        'apps/*/src/**/*.{js,jsx,ts,tsx}',
+        'packages/*/src/**/*.{js,jsx,ts,tsx}',
+      ],
+      exclude: [
+        '**/__generated__/**',
+        '**/__mocks__/**',
+        '**/*.spec.{js,ts,tsx}',
+        '**/*.stories.{js,ts,tsx}',
+        '**/*.test.{js,ts,tsx}',
+        'packages/config/**',
+      ],
+      provider: 'v8',
       reporter: ['lcov'],
     },
   },
