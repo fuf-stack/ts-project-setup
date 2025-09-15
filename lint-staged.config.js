8006a4a761066e366fb0900ca5c33f9905245ac5
Hannes Tiede <951466+toxsick@users.noreply.github.com>
2025-09-15 21:20:16 +0200

    chore: format lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   // eslint-config-fuf
-  'packages/eslint-config-fuf/**/*.{ts}': [
-    'eslint',
-    'vitest related --run',
-  ],
+  'packages/eslint-config-fuf/**/*.{ts}': ['eslint', 'vitest related --run'],
   // config packages
   'packages/**/*.{js,ts}': ['eslint'],
   // other filetypes
